24e7c9bd7dcffa09ff462a5f4cd465a948e97ac1
amadansi26-cmyk <amadansi26@bentleyschool.org>
2026-02-24 09:07:08 -0800

    Update test_602.py

--- test_602.py
+++ test_602.py
@@ -1,45 +1,13 @@
-import os
-
-def toString(fileName):
-    # This function returns the text from a given file.
-    # Any new lines are written as \n
-    base_path = os.path.dirname(__file__)
-    file_path = os.path.join(base_path, fileName)
-
-    with open(file_path, "r") as f:
-        return f.read()
+from CSP_6_02_reading_files import toString, longestLine, toBinary
 
 
-def longestLine(fileName):
-    # Given a file return the longest line from within that file
-    base_path = os.path.dirname(__file__)
-    file_path = os.path.join(base_path, fileName)
-
-    longest = ""
-
-    with open(file_path, "r") as f:
-        for line in f:
-            cleanLine = line.rstrip("\n")
-            if len(cleanLine) > len(longest):
-                longest = cleanLine
-
-    return longest
+def test_toString():
+    assert toString("ExampleText.txt") == "Here is the text\ni am another line\nshort\n"
 
 
-def toBinary(fileName):
-    # Given a file that is only 0's and 1's return a list
-    # broken into groups of 8 (bytes)
-    base_path = os.path.dirname(__file__)
-    file_path = os.path.join(base_path, fileName)
+def test_longestLine():
+    assert longestLine("ExampleText.txt") == "i am another line"
 
-    data = ""
 
-    with open(file_path, "r") as f:
-        for line in f:
-            data += line.strip()
-
-    bytesList = []
-    for i in range(0, len(data), 8):
-        bytesList.append(data[i:i+8])
-
-    return bytesList
+def test_toBinary():
+    assert toBinary("BinaryExample.txt") == ['01101001', '00101010', '1010']
