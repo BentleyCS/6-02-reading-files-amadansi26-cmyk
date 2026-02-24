a9bd12556dc85b316da0786154b260359b8f40af
amadansi26-cmyk <amadansi26@bentleyschool.org>
2026-02-24 08:55:48 -0800

    Update test_602.py

--- test_602.py
+++ test_602.py
@@ -1,4 +1,4 @@
-import fileFunctions as HW
+from CSP_6_02_reading_files (1).py import toString, longestLine, toBinary
 
 
 def test_toString():
@@ -6,16 +6,16 @@ def test_toString():
     # Here is the text
     # i am another line
     # short
-    assert HW.toString("ExampleText.txt") == "Here is the text\ni am another line\nshort\n"
+    assert toString("ExampleText.txt") == "Here is the text\ni am another line\nshort\n"
 
 
 def test_longestLine():
     # Longest line in ExampleText.txt is:
     # i am another line
-    assert HW.longestLine("ExampleText.txt") == "i am another line"
+    assert longestLine("ExampleText.txt") == "i am another line"
 
 
 def test_toBinary():
     # BinaryExample.txt contains:
     # 01101001001010101010
-    assert HW.toBinary("BinaryExample.txt") == ['01101001', '00101010', '1010']
+    assert toBinary("BinaryExample.txt") == ['01101001', '00101010', '1010']
